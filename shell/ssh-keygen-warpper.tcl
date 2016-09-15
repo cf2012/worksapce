bfef071bee85dcaec09150a103018a14ec470f2b
cf2012 <changfeng29@gmail.com>
2016-09-15 22:12:24 +0800

    Update ssh-keygen-warpper.tcl
    不需要设置 $ip. 删除

--- shell/ssh-keygen-warpper.tcl
+++ shell/ssh-keygen-warpper.tcl
@@ -1,9 +1,6 @@
 #!/usr/bin/expect 
-# 作用: 调用 ssh-keygen 生成公私钥. 生成公私钥时,不需要人工按`回车`
+# 作用: 调用 ssh-keygen 生成公私钥. 生成公私钥时,不需要人工按`回车`,便于自动化部署
 # 在CentOS7.2 expect-5.45 中测试通过
-
-set ip   [lindex $argv 0]
-set timeout 30
 
 spawn ssh-keygen
 
