74efc9a6996f6696d9801e6bdf69450fa4984953
cf2012 <changfeng29@gmail.com>
2016-12-11 20:02:14 +0800

    Create dfi.py
    跟 df -i 显示效果相同. 方便监控目录 inode 使用率.

---
+++ dfi/dfi.py
@@ -0,0 +1,15 @@
+#!/bin/env python
+#!coding=utf8
+"""
+输出指定目录inode使用率.  模仿 df -i 
+在 CentOS 6.7 + py2.6 中测试通过
+在 CentOS 6.7 + py3.5 中测试通过
+
+"""
+import os, sys
+
+if __name__ == "__main__":
+	path=sys.argv[1]
+	sfsp=os.statvfs(path)
+	print(round(float(sfsp.f_files - sfsp.f_ffree)/sfsp.f_files * 100, 1) )
+
